ad9dac7d73d6404ad497a0a271ec37de40ddee77
ErinaGarnisWidyawati <erinagarnis@gmail.com>
2021-09-06 22:21:00 +0700

    commit

---
+++ Nilai Variabel.php
@@ -0,0 +1,17 @@
+<?php
+   $nama = "Erina";
+   $sekolah = "SMK Telkom Malang";
+   $umur = 17;
+
+   // menampilkan nilai variabel
+   // contoh 1
+   echo "Hallo namaku $nama usiaku $umur tahun";
+   echo "<br>";
+   echo "Aku sekolah di $sekolah";
+
+   //contoh 2
+   echo "<br> <br>";
+   echo 'Hallo namaku '.$nama.' usiaku '.$umur.' tahun';
+   echo "<br>";
+   echo 'Aku sekolah di '.$sekolah;
+?>
